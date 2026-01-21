b6c728a6f3383391dfa923f9584946197608aeb7
25Keshavk <110709267+25Keshavk@users.noreply.github.com>
2026-01-21 13:46:54 -0800

    Update ratelimit.ts

--- lib/ratelimit.ts
+++ lib/ratelimit.ts
@@ -3,7 +3,7 @@ import { getRedis } from "./redis";
 export async function rateLimit(key: string, limit: number, windowSec: number) {
   const redis = await getRedis();
 
-  // node-redis typing can be string|number depending on config; coerce safely
+  // node-redis typing can be string|number depending on config; Make Note of this error, I spent a while debugging
   const raw = await redis.incr(key);
   const count = typeof raw === "string" ? parseInt(raw, 10) : raw;
 
